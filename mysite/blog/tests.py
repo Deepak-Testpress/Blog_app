0eb8ae564660c47d449bb7d684009cc3489b812e
Deepak-Testpress <deepak@testpress.in>
2022-11-09 17:10:20 +0530

    Refactor Post model test

--- mysite/blog/tests.py
+++ mysite/blog/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
-from django.db import models
-from blog.models import Post, PublishedManager
+from blog.models import Post
 from django.contrib.auth.models import User
 
 
@@ -15,35 +14,18 @@ class PostModelTest(TestCase):
             title="Sample Test",
             body="I am Testing",
             author=testuser,
-            status=Post.STATUS_CHOICES[0][0],
+            status="draft",
         )
         Post.objects.create(
             title="Sample Test 2",
             body="I am Testing 2",
             author=testuser,
-            status=Post.STATUS_CHOICES[1][0],
+            status="published",
         )
-
-    def test_post_getters(self):
-        post = Post.objects.get(id=1)
-        testuser = User.objects.get(id=1)
-        self.assertEqual(str(post.title), "Sample Test")
-        self.assertEqual(str(post.body), "I am Testing")
-        self.assertEqual(str(post.status), "draft")
-        self.assertEqual(post.author, testuser)
-
-    def test_post_field_structure(self):
-        post = Post.objects.get(id=1)
-        title_max_length = post._meta.get_field("title").max_length
-        self.assertEqual(title_max_length, 250)
-        slug_max_length = post._meta.get_field("slug").max_length
-        self.assertEqual(slug_max_length, 250)
-        status_max_length = post._meta.get_field("status").max_length
-        self.assertEqual(status_max_length, 10)
 
     def test_object_name_is_title(self):
         post = Post.objects.get(id=1)
-        self.assertEqual(str(post), "Sample Test")
+        self.assertEqual("Sample Test", str(post))
 
     def test_published_manager(self):
         published_queryset = Post.published.get_queryset()
