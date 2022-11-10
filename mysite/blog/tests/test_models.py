e5349c261394b048612ab3ffee837ef44fa97c5c
Deepak-Testpress <deepak@testpress.in>
2022-11-10 16:40:26 +0530

    Refactor test cases by adding ModelMixin

--- mysite/blog/tests/test_models.py
+++ mysite/blog/tests/test_models.py
@@ -1,52 +1,16 @@
 from django.test import TestCase
-from blog.models import Post
-from django.contrib.auth.models import User
+from blog.tests.test_ModelMixinTestCase import ModelMixinTestCase
 
 
-class PostModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-
-        testuser = User.objects.create_user(username="deepak", password="root")
-        Post.objects.create(
-            title="Sample Test",
-            body="I am Testing",
-            author=testuser,
-            status=Post.STATUS_CHOICES[0][0],
-        )
-        Post.objects.create(
-            title="Sample Test 2",
-            body="I am Testing 2",
-            author=testuser,
-            status=Post.STATUS_CHOICES[1][0],
-        )
-
-    def test_post_getters(self):
-        post = Post.objects.get(id=1)
-        testuser = User.objects.get(id=1)
-        self.assertEqual(str(post.title), "Sample Test")
-        self.assertEqual(str(post.body), "I am Testing")
-        self.assertEqual(str(post.status), "draft")
-        self.assertEqual(post.author, testuser)
-
-    def test_post_field_structure(self):
-        post = Post.objects.get(id=1)
-        title_max_length = post._meta.get_field("title").max_length
-        self.assertEqual(title_max_length, 250)
-        slug_max_length = post._meta.get_field("slug").max_length
-        self.assertEqual(slug_max_length, 250)
-        status_max_length = post._meta.get_field("status").max_length
-        self.assertEqual(status_max_length, 10)
-
+class PostModelTest(ModelMixinTestCase, TestCase):
     def test_object_name_is_title(self):
-        post = Post.objects.get(id=1)
-        self.assertEqual(str(post), "Sample Test")
+        self.assertEqual(
+            str(self.test_draft_post),
+            "Test post thats status=draft by default",
+        )
 
     def test_published_manager(self):
-        published_queryset = Post.published.get_queryset()
-        objects_queryset_with_status_published = Post.objects.filter(
-            status="published"
-        )
         self.assertQuerysetEqual(
-            published_queryset, objects_queryset_with_status_published
+            self.published_queryset,
+            self.post_objects_with_status_published_queryset,
         )
--- mysite/blog/tests/test_views.py
+++ mysite/blog/tests/test_views.py
@@ -1,81 +1,31 @@
-from django.test import TestCase, Client
-from blog.forms import EmailPostForm
+from django.test import TestCase
 from blog.models import Post
-from django.contrib.auth.models import User
 from django.urls import reverse
+from blog.tests.test_ModelMixinTestCase import ModelMixinTestCase
 
 
-class PostViewsTest(TestCase):
-    def setUp(self):
-        self.client = Client()
-
-        self.test_user = User.objects.create_user(
-            username="deepak", password="root"
-        )
-        self.test_post = Post.objects.create(
-            title="Test post thats status=draft by default",
-            author=self.test_user,
-            body="This post is created by testuser author",
-        )
-        self.published_post = Post.objects.create(
-            title="Test post thats status=published",
-            author=self.test_user,
-            body="This post is created by testuser author",
-            slug="post-created-testuser-author",
-            status="published",
-        )
-
+class PostViewsTest(ModelMixinTestCase, TestCase):
     def test_post_list_template_used(self):
-        post_list_url = reverse("blog:post_list")
-        response = self.client.get(post_list_url)
+        self.post_list_url = reverse("blog:post_list")
+        response = self.client.get(self.post_list_url)
 
         self.assertTemplateUsed(response, "blog/post/list.html")
 
-    def test_post_list_should_return_status_200(self):
-        post_list_url = reverse("blog:post_list")
-        response = self.client.get(post_list_url)
+    def test_post_list_should_return_status_200_GET(self):
+        self.post_list_url = reverse("blog:post_list")
+        response = self.client.get(self.post_list_url)
 
         self.assertEqual(response.status_code, 200)
 
     def test_post_detail_template_used(self):
-        published_post = Post.objects.get(id=2)
-
-        year = published_post.publish.year
-        month = published_post.publish.month
-        day = published_post.publish.day
-        slug = published_post.slug
-
-        post_detail_url = (
-            "http://127.0.0.1:8000/blog/{year}/{month}/{day}/{slug}/".format(
-                year=year, month=month, day=day, slug=slug
-            )
-        )
-
-        response = self.client.get(post_detail_url)
+        response = self.client.get(self.post_detail_url)
 
         self.assertTemplateUsed(response, "blog/post/detail.html")
 
     def test_post_detail_should_return_200_for_valid_post(self):
+        response = self.client.get(self.post_detail_url)
 
-        published_post = Post.objects.get(id=2)
-
-        year = published_post.publish.year
-        month = published_post.publish.month
-        day = published_post.publish.day
-        slug = published_post.slug
-
-        post_detail_url = (
-            "http://127.0.0.1:8000/blog/{year}/{month}/{day}/{slug}/".format(
-                year=year, month=month, day=day, slug=slug
-            )
-        )
-
-        successful_response = self.client.get(post_detail_url)
-
-        self.assertEqual(200, successful_response.status_code)
-        self.assertEqual(
-            Post.objects.first().title, "Test post thats status=published"
-        )
+        self.assertEqual(200, response.status_code)
 
     def test_post_detail_should_return_404_for_invalid_post(self):
 
@@ -97,8 +47,6 @@ class PostViewsTest(TestCase):
         self.assertEqual(404, unsuccessful_response.status_code)
 
     def test_post_share_template_used(self):
-
-        post_share_url = reverse("blog:post_share", args=[2])
-        response = self.client.get(post_share_url)
+        response = self.client.get(self.post_share_url)
 
         self.assertTemplateUsed(response, "blog/post/share.html")
