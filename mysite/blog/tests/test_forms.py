ec08414f1eb2417737e4e8be81b724710648987c
Deepak-Testpress <deepak@testpress.in>
2022-11-11 16:41:34 +0530

    Add test cases for comment support

--- mysite/blog/tests/test_forms.py
+++ mysite/blog/tests/test_forms.py
@@ -1,23 +1,24 @@
 from django.test import TestCase
-from blog.forms import EmailPostForm
-from blog.views import post_share
-from blog.models import Post
+from blog.forms import CommentForm, EmailPostForm
+from blog.views import post_share, post_detail
+from blog.models import Post, Comment
 from django.contrib.auth.models import User
 from django.http import HttpRequest
+from blog.tests.test_ModelMixinTestCase import ModelMixinTestCase
 
 
-class TestForms(TestCase):
-    @classmethod
-    def setUpTestData(cls):
+class TestForms(ModelMixinTestCase, TestCase):
+    # @classmethod
+    # def setUpTestData(cls):
 
-        testuser = User.objects.create_user(username="deepak", password="root")
-        Post.objects.create(
-            title="Sample Test",
-            body="I am Testing",
-            author=testuser,
-            status="published",
-            slug="slug",
-        )
+    #     testuser = User.objects.create_user(username="deepak", password="root")
+    #     Post.objects.create(
+    #         title="Sample Test",
+    #         body="I am Testing",
+    #         author=testuser,
+    #         status="published",
+    #         slug="slug",
+    #     )
 
     def test_email_form_valid_data(self):
         form = EmailPostForm(
@@ -51,6 +52,32 @@ class TestForms(TestCase):
         self.assertRaises(ValueError)
         self.assertRaises(KeyError)
 
+    def test_comment_form_valid_data(self):
+        form = CommentForm(
+            data={
+                "post": self.test_published_post,
+                "name": "First comment",
+                "email": "deeepak@testpress.in",
+                "body": "this is comment body",
+            }
+        )
+        self.assertTrue(form.is_valid())
+
+    def test_comment_form_invalid_data(self):
+        form = CommentForm(
+            data={
+                "post": self.test_published_post,
+                "name": "First comment",
+                "email": "invalid-email",
+                "body": "this is comment body",
+            }
+        )
+        self.assertFalse(form.is_valid())
+
+    def test_comment_form_no_data(self):
+        form = CommentForm(data={})
+        self.assertFalse(form.is_valid())
+
     def test_post_share_should_return_200_with_valid_post_obj(self):
 
         request = HttpRequest()
@@ -65,6 +92,34 @@ class TestForms(TestCase):
             "to": "deepakadishankar@gmail.com",
             "comments": "The post was awesome",
         }
-        post_id = 1
-        res = post_share(request=request, post_id=post_id)
-        self.assertEqual(200, res.status_code)
+        post_id = self.test_published_post.id
+        response = post_share(request=request, post_id=post_id)
+        self.assertEqual(200, response.status_code)
+
+    def test_post_detail_should_return_200_with_valid_post_obj(self):
+
+        request = HttpRequest()
+        request.method = "POST"
+        request.META = {
+            "SERVER_NAME": "testserver",
+            "SERVER_PORT": "8000",
+        }
+        request.POST = {
+            "post": self.test_published_post,
+            "name": "Test comment",
+            "email": "deepak@testpress.in",
+            "body": "this is comment body",
+        }
+
+        response = post_detail(
+            request=request,
+            year=self.test_published_post.publish.year,
+            month=self.test_published_post.publish.month,
+            day=self.test_published_post.publish.day,
+            post=self.test_published_post.slug,
+        )
+        comment = self.test_post_comment
+        post = self.test_published_post
+
+        self.assertEqual(post, comment.post)
+        self.assertEqual(200, response.status_code)
--- mysite/blog/tests/test_models.py
+++ mysite/blog/tests/test_models.py
@@ -3,7 +3,7 @@ from blog.tests.test_ModelMixinTestCase import ModelMixinTestCase
 
 
 class PostModelTest(ModelMixinTestCase, TestCase):
-    def test_object_name_is_title(self):
+    def test_post_object_name_is_title(self):
         self.assertEqual(
             str(self.test_draft_post),
             "Test post thats status=draft by default",
@@ -13,4 +13,12 @@ class PostModelTest(ModelMixinTestCase, TestCase):
         self.assertQuerysetEqual(
             self.published_queryset,
             self.post_objects_with_status_published_queryset,
+        )
+
+
+class CommentModelTest(ModelMixinTestCase, TestCase):
+    def test_comment_object_name_is_title(self):
+        self.assertEqual(
+            str(self.test_post_comment),
+            f"Comment by {self.test_post_comment.name} on {self.test_post_comment.post}",
         )
