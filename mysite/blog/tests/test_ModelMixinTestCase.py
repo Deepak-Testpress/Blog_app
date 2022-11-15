94f6b576a6665f9185f62d4317ff16a975351590
Deepak-Testpress <deepak@testpress.in>
2022-11-15 13:20:51 +0530

    Add test cases for Post model
    - Added test cases to check get_top_four_similar_posts() returns appropriate querysets

--- mysite/blog/tests/test_ModelMixinTestCase.py
+++ mysite/blog/tests/test_ModelMixinTestCase.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from blog.models import Post, Comment
 from django.urls import reverse
+from taggit.managers import TaggableManager
 from django.contrib.auth.models import User
 
 
@@ -29,6 +30,60 @@ class ModelMixinTestCase(TestCase):
             slug="post-created-testuser-author",
             status="published",
         )
+        self.test_first_published_post_with_tag = Post.objects.create(
+            title="First post that will have tag",
+            author=self.test_user,
+            body="This post is created by testuser author",
+            slug="second-post-with-tag",
+            status="published",
+        )
+        self.test_first_published_post_with_tag.tags.add("test")
+
+        self.test_second_published_post_with_tag = Post.objects.create(
+            title="Second Test post that will have tag",
+            author=self.test_user,
+            body="This post is created by testuser author",
+            slug="second-post-with-tag",
+            status="published",
+        )
+
+        # self.test_second_published_post_with_tag.tags.add("poda")
+
+        # self.published_post3_with_tag = Post.objects.create(
+        #     title="Third Test post thats tag=eco",
+        #     author=self.test_user,
+        #     body="This post is created by testuser author",
+        #     slug="third-post-with-tag",
+        #     status="published",
+        # )
+        # self.published_post3_with_tag.tags.add("echo")
+
+        # self.published_post4_with_tag = Post.objects.create(
+        #     title="Fourth Test post thats tag=eco",
+        #     author=self.test_user,
+        #     body="This post is created by testuser author",
+        #     slug="fourth-post-with-tag",
+        #     status="published",
+        # )
+        # self.published_post4_with_tag.tags.add("echo","echo2")
+
+        # self.published_post5_with_tag = Post.objects.create(
+        #     title="Fifth Test post thats tag=eco",
+        #     author=self.test_user,
+        #     body="This post is created by testuser author",
+        #     slug="fifth-post-with-tag",
+        #     status="published",
+        # )
+        # self.published_post5_with_tag.tags.add("echo")
+
+        # self.published_post6_with_tag = Post.objects.create(
+        #     title="Sixth Test post thats tag=eco",
+        #     author=self.test_user,
+        #     body="This post is created by testuser author",
+        #     slug="sixth-post-with-tag",
+        #     status="published",
+        # )
+        # self.published_post6_with_tag.tags.add("echo")
 
         self.post_list_url = reverse("blog:post_list")
         self.post_detail_url = reverse(
--- mysite/blog/tests/test_models.py
+++ mysite/blog/tests/test_models.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from blog.tests.test_ModelMixinTestCase import ModelMixinTestCase
+from django.db.models.query import EmptyQuerySet
+from django.db.models import Model
+from blog.models import Post
 
 
 class PostModelTest(ModelMixinTestCase, TestCase):
@@ -13,6 +16,26 @@ class PostModelTest(ModelMixinTestCase, TestCase):
         self.assertQuerysetEqual(
             self.published_queryset,
             self.post_objects_with_status_published_queryset,
+        )
+
+    def test_get_top_four_similar_posts_returns_empty_queryset_with_only_one_tagged_post(
+        self,
+    ):
+
+        similar_posts = (
+            self.test_first_published_post_with_tag.get_top_four_similar_posts()
+        )
+        self.assertFalse(similar_posts.exists())
+
+    def test_get_top_four_similar_posts_returns_posts_queryset_with_posts_with_same_tag_excluding_self_post(
+        self,
+    ):
+        self.test_second_published_post_with_tag.tags.add("test")
+        similar_posts = (
+            self.test_first_published_post_with_tag.get_top_four_similar_posts()
+        )
+        self.assertEqual(
+            self.test_second_published_post_with_tag, similar_posts.first()
         )
 
 
